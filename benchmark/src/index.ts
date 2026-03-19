69f1dac6682c581d93bd680aa21491480bd2a815
byc <2786660867@qq.com>
2026-03-19 22:53:18 +0800

    feat(benchmark): 优化基准测试数据结构

--- benchmark/src/index.ts
+++ benchmark/src/index.ts
@@ -3,57 +3,35 @@ import { $$find } from '@jsonjoy.com/json-pointer/lib/codegen/index.js';
 import { Bench } from 'tinybench';
 import { $seek, seek, peek } from 'volta-json-ptr';
 
-const bench = new Bench({ name: 'Benchmark', iterations: 10000 });
+const testData = {
+  shallow: { a: 'value', b: 123, c: true },
+  deep: { a: { b: { c: { d: { e: 'deep value' } } } } },
+  array: { items: [{ id: 1, name: 'Item 1' }] },
+};
+
+const bench = new Bench({ name: 'Benchmark', iterations: 100, time: 100 });
 
 bench.add('volta-json-ptr - Runtime', () => {
-  const testData = {
-    shallow: { a: 'value', b: 123, c: true },
-    deep: { a: { b: { c: { d: { e: 'deep value' } } } } },
-    array: { items: [{ id: 1, name: 'Item 1' }] },
-  };
-
   const _tmp = $seek(testData, '/deep/a/b/c/d/e');
 });
 
 bench.add('volta-json-ptr - Compiled', () => {
-  const testData = {
-    shallow: { a: 'value', b: 123, c: true },
-    deep: { a: { b: { c: { d: { e: 'deep value' } } } } },
-    array: { items: [{ id: 1, name: 'Item 1' }] },
-  };
-
-  const _tmp = seek(testData, '/deep/a/b/c/d/e/');
+  const _tmp = seek(testData, '/deep/a/b/c/d/e');
 });
 
 bench.add('volta-json-ptr - JIT', () => {
-  const testData = {
-    shallow: { a: 'value', b: 123, c: true },
-    deep: { a: { b: { c: { d: { e: 'deep value' } } } } },
-    array: { items: [{ id: 1, name: 'Item 1' }] },
-  };
-
   const _tmp = peek(testData, '/deep/a/b/c/d/e');
 });
 
 bench.add('@jsonjoy.com/json-pointer', () => {
-  const testData = {
-    shallow: { a: 'value', b: 123, c: true },
-    deep: { a: { b: { c: { d: { e: 'deep value' } } } } },
-    array: { items: [{ id: 1, name: 'Item 1' }] },
-  };
   const _tmp = findByPointer('/deep/a/b/c/d/e', testData);
 });
 
 const find = $$find(['deep', 'a', 'b', 'c', 'd', 'e']);
-const $find = eval(find);
 
-bench.add('@jsonjoy.com/json-pointer -JWT', () => {
-  const testData = {
-    shallow: { a: 'value', b: 123, c: true },
-    deep: { a: { b: { c: { d: { e: 'deep value' } } } } },
-    array: { items: [{ id: 1, name: 'Item 1' }] },
-  };
+let $find = new Function(`return ${find}`)();
 
+bench.add('@jsonjoy.com/json-pointer - JIT', () => {
   const _tmp = $find(testData);
 });
 
